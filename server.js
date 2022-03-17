812244d21ed5572c53d36b8d706fa6ea47b1ab03
Moises <moises109811@gmail.com>
2022-03-16 23:21:18 -0700

    + gitignore, server.js get routes, + to db

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const fs = require('fs');
+const path = require("path");
+const database = require("./db/db.json");
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Get files in "public" folder
+app.use(express.static("public"));
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// GET Route for homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+
+
+//Reads the db.json file and return all saved notes as JSON.
+app.route("/api/notes")
+  .get(function (req, res) {
+    res.json(database);
+  })
+  // Adds new note to the json db file.
+  .post(function (req, res) {
+    let jsonFilePath = path.join(__dirname, "/db/db.json");
+    let newNote = req.body;
+    // Write the db.json file again.
+    fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("New Note has been saved!");
+    });
+    // Response
+    res.json(newNote);
+  });
+
+  app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
