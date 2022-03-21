371303ae39f53f4ee6d7193418ef31c2e6bb8dbc
Moises <moises109811@gmail.com>
2022-03-21 13:21:37 -0700

    added uuid and fixed routing

--- routes/notes.js
+++ routes/notes.js
@@ -1,23 +1,23 @@
-const fb = require('express').Router();
+const db = require('express').Router();
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
 // GET Route for retrieving all the notes
-fb.get('/', (req, res) =>
+db.get('/', (req, res) =>
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
 // POST Route for saving notes
-fb.post('/', (req, res) => {
+db.post('/', (req, res) => {
   // Destructuring assignment for the items in req.body
-  const { email, feedbackType, feedback } = req.body;
+  const { title, text } = req.body;
 
   // If all the required properties are present
   if (title && text) {
-    readAndAppend(newFeedback, './db/db.json');
+    readAndAppend(newNote, './db/db.json');
 
     const response = {
       status: 'success',
-      body: newFeedback,
+      body: newNote,
     };
 
     res.json(response);
@@ -26,4 +26,4 @@ fb.post('/', (req, res) => {
   }
 });
 
-module.exports = fb;
+module.exports = db;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const path = require("path");
-const database = require("./db/db.json");
-const api = require('./routes/index.js');
+const { v4: uuidv4 } = require('uuid');
+
+// const api = require('./routes/index.js');
 
 
 const app = express();
@@ -12,15 +13,13 @@ const PORT = process.env.PORT || 3001;
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api', api);
+// app.use('/api', api);
 
 // Get files in "public" folder
 app.use(express.static("public"));
 
 // GET Route for homepage
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+
 
 // GET Route for notes page
 app.get('/notes', (req, res) =>
@@ -32,12 +31,16 @@ app.get('/notes', (req, res) =>
 //Reads the db.json file and return all saved notes as JSON.
 app.route("/api/notes")
   .get(function (req, res) {
+    let database = JSON.parse(fs.readFileSync("db/db.json"))
     res.json(database);
   })
   // Adds new note to the json db file.
   .post(function (req, res) {
     let jsonFilePath = path.join(__dirname, "/db/db.json");
+    let database = JSON.parse(fs.readFileSync("db/db.json"))
     let newNote = req.body;
+    newNote.id = uuidv4()
+    database.push (newNote)
     // Write the db.json file again.
     fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
       if (err) {
@@ -48,6 +51,10 @@ app.route("/api/notes")
     // Response
     res.json(newNote);
   });
+
+  app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
   app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
